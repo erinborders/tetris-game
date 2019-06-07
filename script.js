c6250c011cafbef7bd63b3dd0e847543a3cc33ae
Erin Borders <borders16@gmail.com>
2019-06-06 22:22:55 -0400

    made a representation of the t piece with and now preparing to draw it

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const context = canvas.getContext('2d');
 //to make sure this works, we're going to paint the canvas
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+//making the t block in tetris --> we're going to represent this with a 2 dimensional matrix
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1,],
+    [0, 1, 0],
+]
+
+//now i want to draw this first piece
+matrix.forEach((row, y) => {
+    
+}
